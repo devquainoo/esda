dfe4022a45c03a71d98b333b16d12c2f0a084e54
gideon <qgdeon@live.com>
2020-04-29 22:08:42 +0800

    commented out pypaystack import

--- payment/views.py
+++ payment/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from pypaystack import Transaction, Customer, Plan
+# from pypaystack import Transaction, Customer, Plan
 from django.conf import settings
 
 # Create your views here.
@@ -8,10 +8,10 @@ def charge(request):
     return render(request, 'payment/checkout.html')
 
 def verify(request, reference):
-    transaction = Transaction(settings.SK_KEY)
-    response = transaction.verify(reference)
-    if response[3]['status'] == 'success':
-        return render(request,'pages/success.html')
-    else:
-        return HttpResponse('failed')
+    # transaction = Transaction(settings.SK_KEY)
+    # response = transaction.verify(reference)
+    # if response[3]['status'] == 'success':
+    return render(request,'pages/success.html',{'reference':reference})
+    # else:
+        # return HttpResponse('failed')
     # return HttpResponse(response)
